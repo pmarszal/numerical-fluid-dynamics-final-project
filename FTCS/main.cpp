a86bafd55ad33cdc302fb5f8146af5e05e6cb878
Philip <marszal@J.C.Maxwell>
2016-07-31 11:10:51 +0200

    Examined the stability of various dt's depending on Pe

--- FTCS/main.cpp
+++ FTCS/main.cpp
@@ -136,12 +136,12 @@ int main(int argc, char ** argv){
 		for(int n=0; n < t_fin/dt; n++){
 			FTCS(T,u_0,v_0);
 			if(i_t<i_t_max){
-				if( (n+1)*dt > t_snap[i_t] && (n+1)*dt<=t_snap[i_t+1]){
+				if( (n+1)*dt >= t_snap[i_t] && (n+1)*dt<t_snap[i_t+1]){
 					ostringstream snap_name;
 					snap_name <<dirname<< (n+1)*dt << "_" << Pe << "_"<< Nx<<"_"<<Ny<<"_"<<dt<<"_"<<b_Q<<".txt";
 					save_data(T, snap_name.str().c_str());
 				}
-				if((n+1)*dt>t_snap[i_t]){
+				if((n+1)*dt>=t_snap[i_t]){
 					i_t+=1;
 				}
 			}
@@ -154,12 +154,12 @@ int main(int argc, char ** argv){
 			outfile << n*dt << " " << SSE(T,T_star)<<endl;
 			FTCS_with_Q(T,u_0,v_0, Q);
 			if(i_t<i_t_max-1){
-				if( (n+1)*dt > t_snap[i_t] && (n+1)*dt<=t_snap[i_t+1]){
+				if( (n+1)*dt >= t_snap[i_t] && (n+1)*dt<t_snap[i_t+1]){
 					ostringstream snap_name;
 					snap_name <<dirname<< (n+1)*dt << "_" << Pe << "_"<< Nx<<"_"<<Ny<<"_"<<dt<<"_"<<b_Q<<".txt";
 					save_data(T, snap_name.str().c_str());
 				}
-				if((n+1)*dt>t_snap[i_t]){
+				if((n+1)*dt>=t_snap[i_t]){
 					i_t+=1;
 				}
 			}
@@ -168,9 +168,8 @@ int main(int argc, char ** argv){
 		outfile.close();
 	}
 	ostringstream snap_name;
-	snap_name <<dirname<< t_fin/dt -dt << "_" << Pe << "_"<< Nx<<"_"<<Ny<<"_"<<dt<<"_"<<b_Q<<".txt";
+	snap_name <<dirname<< t_fin << "_" << Pe << "_"<< Nx<<"_"<<Ny<<"_"<<dt<<"_"<<b_Q<<".txt";
 	save_data(T, snap_name.str().c_str());
-	print_array(T_star);
 	return 0;
 }
 
